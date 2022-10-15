191c737955dc4942d58d603034036394256482d7
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-10-14 20:35:33 -0700

    feat: add version_info

--- discatcore/__init__.py
+++ discatcore/__init__.py
@@ -5,16 +5,33 @@ __author__ = "EmreTech"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+from typing import Literal, NamedTuple
+
 from . import gateway, http, types, utils
 from .errors import *
 from .file import *
 from .impl import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    patch: int
+    release_level: Literal["alpha", "beta", "release candidate", "final"]
+    build_metadata: int
+
+
+version_info: VersionInfo = VersionInfo(
+    major=0, minor=1, patch=0, release_level="alpha", build_metadata=0
+)
 
 __all__ = (
     "gateway",
     "http",
     "types",
     "utils",
+    "VersionInfo",
+    "version_info",
 )
 __all__ += errors.__all__
 __all__ += file.__all__
