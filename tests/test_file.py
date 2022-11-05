a54eb4d58aa7c71c08edd55752904d4f386945e5
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-11-05 09:51:01 -0700

    fix(test_file): application/plain is not valid

--- tests/test_file.py
+++ tests/test_file.py
@@ -8,7 +8,7 @@ from discatcore import BasicFile
 class TestBasicFile:
     def test_explicit_spoiler(self):
         buffer = io.StringIO("hello there :D")
-        file = BasicFile(buffer, "application/plain", filename="hello.txt", spoiler=True)
+        file = BasicFile(buffer, "text/plain", filename="hello.txt", spoiler=True)
         assert file.spoiler
 
     def test_implicit_spoiler(self):
