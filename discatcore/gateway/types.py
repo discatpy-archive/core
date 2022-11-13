3338336c47de75580498f9c77bc71f7f32534892
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-11-13 15:32:21 -0800

    feat(gateway.types)!: remove unused convert_from_untyped function

--- discatcore/gateway/types.py
+++ discatcore/gateway/types.py
@@ -15,7 +15,6 @@ __all__ = (
     "BinaryTypedWSMessage",
     "is_text",
     "is_binary",
-    "convert_from_untyped",
 )
 
 
@@ -44,13 +43,3 @@ def is_text(base: BaseTypedWSMessage[t.Any]) -> TypeGuard[TextTypedWSMessage]:
 
 def is_binary(base: BaseTypedWSMessage[t.Any]) -> TypeGuard[BinaryTypedWSMessage]:
     return base.type is aiohttp.WSMsgType.BINARY
-
-
-def convert_from_untyped(msg: aiohttp.WSMessage) -> BaseTypedWSMessage[t.Any]:
-    base: BaseTypedWSMessage[t.Any] = BaseTypedWSMessage.convert_from_untyped(msg)
-
-    if base.type == aiohttp.WSMsgType.TEXT:
-        return t.cast(TextTypedWSMessage, base)
-    elif base.type == aiohttp.WSMsgType.BINARY:
-        return t.cast(BinaryTypedWSMessage, base)
-    return base
