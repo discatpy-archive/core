7bbb4e73065046492071bb7078f108db47ecbbc7
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-11-13 17:22:16 -0800

    refactor(test_file): merge explicit_spoiler & implicit_spoiler

--- tests/test_file.py
+++ tests/test_file.py
@@ -6,12 +6,11 @@ from discatcore import BasicFile
 
 
 class TestBasicFile:
-    def test_explicit_spoiler(self):
+    def test_spoiler(self):
         buffer = io.StringIO("hello there :D")
+
         file = BasicFile(buffer, "text/plain", filename="hello.txt", spoiler=True)
         assert file.spoiler
 
-    def test_implicit_spoiler(self):
-        buffer = io.StringIO("hello there :D")
         file = BasicFile(buffer, "text/plain", filename="SPOILER_hello.txt")
         assert file.spoiler
