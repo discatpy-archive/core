895f0aefdc13ce799485ef03f1e774a7a01c5077
EmreTech <50607143+EmreTech@users.noreply.github.com>
2022-12-01 21:35:31 -0800

    refactor!: rewrite dispatcher & event

--- discatcore/utils/dispatcher.py
+++ discatcore/utils/dispatcher.py
@@ -1,162 +1,228 @@
 # SPDX-License-Identifier: MIT
 
+from __future__ import annotations
+
 import asyncio
 import inspect
 import logging
+import sys
 import traceback
 import typing as t
-from collections.abc import Callable, Coroutine
+from collections import defaultdict
 
-from .event import Event
+import attr
+from typing_extensions import Self, TypeGuard
+
+from .event import Event, EventT, ExceptionEvent
+from .json import JSONObject
+
+if t.TYPE_CHECKING:
+    from ..gateway import GatewayClient
+
+if sys.version_info >= (3, 10):
+    from types import UnionType
+
+    _union_types = {t.Union, UnionType}
+else:
+    _union_types = {t.Union}
 
 _log = logging.getLogger(__name__)
 
-__all__ = ("Dispatcher",)
+__all__ = (
+    "Consumer",
+    "consumer_for",
+    "Dispatcher",
+)
 
 T = t.TypeVar("T")
-Func = Callable[..., T]
-CoroFunc = Func[Coroutine[t.Any, t.Any, t.Any]]
+DispatcherT = t.TypeVar("DispatcherT", bound="Dispatcher")
+ListenerCallbackT = t.TypeVar("ListenerCallbackT", bound="ListenerCallback[Event]")
+Coro = t.Coroutine[T, t.Any, t.Any]
+
+ListenerCallback = t.Callable[[EventT], Coro[None]]
+ConsumerCallback = t.Callable[[DispatcherT, GatewayClient, JSONObject], Coro[None]]
+
+
+@attr.define
+class Consumer(t.Generic[DispatcherT]):
+    """Represents a dispatcher consumer. A consumer consumes a raw event and performs actions based on the raw event."""
+
+    callback: ConsumerCallback[DispatcherT]
+    events: tuple[type[Event], ...]
+
+
+def consumer_for(
+    *event_types: type[Event],
+) -> t.Callable[[ConsumerCallback[DispatcherT]], Consumer[DispatcherT]]:
+    event_types = tuple({event for event_type in event_types for event in event_type.dispatches})
+
+    def wrapper(func: ConsumerCallback[DispatcherT]) -> Consumer[DispatcherT]:
+        return Consumer(func, event_types)
+
+    return wrapper
+
+
+def _is_exception_event(e: EventT) -> TypeGuard[ExceptionEvent[EventT]]:
+    return isinstance(e, ExceptionEvent)
 
 
 class Dispatcher:
-    """A class that helps manage events.
+    """A class that helps manage events."""
 
-    Attributes:
-        events (dict[str, Event]): The callbacks for each event.
-    """
-
-    __slots__ = ("events",)
+    __slots__ = ("_listeners", "_consumers")
 
     def __init__(self) -> None:
-        self.events: dict[str, Event] = {}
+        self._listeners: defaultdict[type[Event], list[ListenerCallback[Event]]] = defaultdict(
+            lambda: []
+        )
+        self._consumers: dict[str, Consumer[Self]] = {}
 
-    def get_event(self, name: str) -> t.Optional[Event]:
-        """Returns an event with the name provided.
+        for name, value in inspect.getmembers(self):
+            if not isinstance(value, Consumer):
+                continue
 
-        Args:
-            name (str): The name of the event that will be returned.
+            self._consumers[name.lower()] = value
 
-        Returns:
-            The event, none if not found.
-        """
-        return self.events.get(name)
+    async def _run_listener(self, event: EventT, listener: ListenerCallback[EventT]) -> None:
+        try:
+            await listener(event)
+        except asyncio.CancelledError:
+            pass
+        except Exception as e:
+            if _is_exception_event(event):
+                _log.error(
+                    "There was an error while running the listener callback (%s%s) under exception event %s.%s: %s",
+                    listener.__name__,
+                    inspect.signature(listener),
+                    type(event).__module__,
+                    type(event).__qualname__,
+                    traceback.format_exception(type(e), e, e.__traceback__),
+                )
+            else:
+                exec_event = ExceptionEvent(
+                    exception=e, failed_event=event, failed_listener=listener
+                )
 
-    def new_event(self, name: str) -> Event:
-        """Creates a new event. Returns this new event after creation.
+                _log.info(
+                    "An exception occured while handling %s.%s.",
+                    type(event).__module__,
+                    type(event).__qualname__,
+                )
+                await self.dispatch(exec_event)
 
-        Args:
-            name (str): The name of the new event.
+    async def _handle_consumer(
+        self, consumer: ConsumerCallback[Self], gateway: GatewayClient, payload: JSONObject
+    ):
+        try:
+            await consumer(self, gateway, payload)
+        except asyncio.CancelledError:
+            pass
+        except Exception as e:
+            asyncio.get_running_loop().call_exception_handler(
+                {
+                    "message": "An exception occured while consuming a raw event.",
+                    "exception": e,
+                    "task": asyncio.current_task(),
+                }
+            )
 
-        Returns:
-            The new event created.
-        """
-        new_event = Event(name, self)
-        self.events[name] = new_event
-        return new_event
+    def subscribe(self, event: type[EventT], func: ListenerCallback[EventT]) -> None:
+        if not asyncio.iscoroutinefunction(func):
+            raise TypeError(f"listener callback {func.__name__!r} has to be a coroutine function!")
 
-    def add_event(self, event: Event, *, override: bool = True) -> None:
-        """Adds a new pre-existing event.
+        _log.debug(
+            "Subscribing listener callback (%s%s) to event %s.%s",
+            func.__name__,
+            inspect.signature(func),
+            event.__module__,
+            event.__qualname__,
+        )
+        self._listeners[event].append(func)  # pyright: ignore
 
-        Args:
-            event (Event): The event to add.
-        """
-        if self.has_event(event.name) and not override:
+    def unsubscribe(self, event: type[EventT], func: ListenerCallback[EventT]) -> None:
+        listeners = self._listeners.get(event)
+        if not listeners:
             return
 
-        self.events[event.name] = event
+        _log.debug(
+            "Unsubscribing listener callback (%s%s) from event %s.%s",
+            func.__name__,
+            inspect.signature(func),
+            event.__module__,
+            event.__qualname__,
+        )
+        listeners.remove(func)  # pyright: ignore
 
-    def remove_event(self, name: str) -> None:
-        """Removes an event.
+        if not listeners:
+            del self._listeners[event]
 
-        Args:
-            name (str): The name of the event to remove.
-        """
-        if name not in self.events:
-            raise ValueError(f"There is no event with name {name}!")
+    def listen_to(self, *events: type[Event]) -> t.Callable[[ListenerCallbackT], ListenerCallbackT]:
+        def wrapper(func: ListenerCallbackT) -> ListenerCallbackT:
+            func_sig = inspect.signature(func)
+            event_arg = next(iter(func_sig.parameters.values()))
+            event_arg_anno = event_arg.annotation
 
-        del self.events[name]
-        _log.debug("Removed event with name %s", name)
-
-    def has_event(self, name: str) -> bool:
-        """Check if this dispatcher already has a event.
-
-        Args:
-            name (str): The name of the event to find.
-
-        Returns:
-            A bool correlating to if there is a event with that name or not.
-        """
-        return name in self.events
-
-    def callback_for(
-        self, event: str, *, one_shot: bool = False, force_parent: bool = False
-    ) -> Callable[[CoroFunc], Event]:
-        """A shortcut decorator to add a callback to an event.
-        If the event does not exist already, then a new one will be created.
-
-        Args:
-            event: The name of the event to get or create.
-            one_shot: Whether or not the callback should be a one shot (which means the callback will be removed after running). Defaults to False.
-            force_parent: Whether or not this callback contains a self parameter. Defaults to False.
-
-        Returns:
-            A wrapper function that acts as the actual decorator.
-        """
-
-        def wrapper(coro: CoroFunc):
-            if not self.has_event(event):
-                event_cls = self.new_event(event)
+            resolved_events: set[type[Event]]
+            if event_arg_anno is inspect.Parameter.empty:
+                if events:
+                    resolved_events = set(events)
+                else:
+                    raise TypeError(
+                        "No event type was provided! Please provide it as an argument or a type hint."
+                    )
             else:
-                event_cls = self.events[event]
 
-            event_cls.add_callback(coro, one_shot=one_shot, force_parent=force_parent)
-            return event_cls
+                def event_check(arg: t.Any) -> None:
+                    if not isinstance(arg, type) or not issubclass(arg, Event):
+                        raise TypeError(f"Expected an event, got {arg!r}.")
+
+                if t.get_origin(event_arg_anno) in _union_types:
+                    union_args = t.get_args(event_arg_anno)
+
+                    for arg in union_args:
+                        event_check(arg)
+
+                    resolved_events = t.cast(set[type[Event]], set(union_args))
+                else:
+                    event_check(event_arg_anno)
+                    resolved_events = {t.cast(type[Event], event_arg_anno)}
+
+            for event in resolved_events:
+                self.subscribe(event, func)
+
+            return func
 
         return wrapper
 
-    # global error handler
+    def dispatch(self, event: Event) -> asyncio.Future[t.Any]:
+        _log.debug(
+            "Dispatching event %s.%s (which dispatches event(s) %r).",
+            type(event).__module__,
+            type(event).__qualname__,
+            [f"{e.__module__}.{e.__qualname__}" for e in event.dispatches],
+        )
+        dispatched: t.List[Coro[None]] = []
 
-    async def error_handler(self, exception: Exception) -> None:
-        """Basic error handler for dispatched events.
+        for event_type in event.dispatches:
+            for listener in self._listeners.get(event_type, []):
+                dispatched.append(self._run_listener(event, listener))
 
-        Args:
-            exception (Exception): The exception from the dispatched event.
-        """
-        traceback.print_exception(type(exception), exception, exception.__traceback__)
+        def _completed_future() -> asyncio.Future[None]:
+            future = asyncio.get_running_loop().create_future()
+            future.set_result(None)
+            return future
 
-    def override_error_handler(self, func: CoroFunc) -> None:
-        """Overrides a new error handler for dispatched events.
+        return asyncio.gather(*dispatched) if dispatched else _completed_future()
 
-        Args:
-            func (CoroFunc): The new error handler.
-        """
-        if not asyncio.iscoroutinefunction(func):
-            raise TypeError("Callback provided is not a coroutine.")
+    def consume(self, event: str, gateway: GatewayClient, payload: JSONObject):
+        consumer = self._consumers.get(event)
 
-        orig_handler_sig = inspect.signature(self.error_handler)
-        new_handler_sig = inspect.signature(func)
+        if not consumer:
+            _log.info("Consumer %s does not exist. Skipping consumption.", event)
+            return
 
-        if orig_handler_sig.parameters != new_handler_sig.parameters:
-            raise TypeError(
-                "Overloaded error handler does not have the same parameters as original error handler."
-            )
-
-        setattr(self, "error_handler", func)
-        _log.debug("Registered new error handler")
-
-    # dispatch
-
-    def dispatch(self, name: str, *args: t.Any, **kwargs: t.Any) -> None:
-        """Dispatches a event. This will trigger the all of the event's
-        callbacks.
-
-        Args:
-            name (str): The name of the event to dispatch.
-            *args (t.Any): Arguments to pass into the event.
-            **kwargs (t.Any): Keyword arguments to pass into the event.
-        """
-        _log.debug("Dispatching event %s", name)
-        event = self.events.get(name)
-
-        if event is not None:
-            event.dispatch(*args, **kwargs)
+        _log.debug("Consuming raw event %s.", event)
+        asyncio.create_task(
+            self._handle_consumer(consumer.callback, gateway, payload),
+            name=f"DisCatCore Consumer {event}",
+        )
