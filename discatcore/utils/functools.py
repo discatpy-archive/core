e0ba017859a9a655a4a7d1af5e5752a3f3f82a37
Emre Terzioglu <50607143+EmreTech@users.noreply.github.com>
2023-07-16 15:16:32 -0700

    refactor: update classproperty type hints

--- discatcore/utils/functools.py
+++ discatcore/utils/functools.py
@@ -7,19 +7,20 @@ from typing_extensions import Self
 __all__ = ("classproperty",)
 
 T = t.TypeVar("T")
+ClsT = t.TypeVar("ClsT")
 
 
-class classproperty(t.Generic[T]):
-    def __init__(self, fget: t.Callable[[t.Any], T], /) -> None:
-        self.fget: "classmethod[T]"
+class classproperty(t.Generic[ClsT, T]):
+    def __init__(self, fget: t.Callable[[ClsT], T], /) -> None:
+        self.fget: "classmethod[ClsT, ..., T]"
         self.getter(fget)
 
-    def getter(self, fget: t.Callable[[t.Any], T], /) -> Self:
+    def getter(self, fget: t.Callable[[ClsT], T], /) -> Self:
         if not isinstance(fget, classmethod):
             raise ValueError(f"Callable {fget.__name__} is not a classmethod!")
 
         self.fget = fget
         return self
 
-    def __get__(self, obj: t.Optional[t.Any], type: t.Optional[type]) -> T:
+    def __get__(self, obj: t.Optional[t.Any], type: t.Optional[ClsT]) -> T:
         return self.fget.__func__(type)
